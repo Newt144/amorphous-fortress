8194881b5a5015b83aa8c8a84bf2ef41cea4bb92
Newt144 <newt144@gmail.com>
2018-03-21 10:47:26 -0500

    Add singleton to hold globally accessible player information; Add custom component FaceCursor that causes the gameObject to turn towards the cursor at a given speed; Move old control scripts to deprecated folder; Yes I now realize I should've committed all of these separately sorry

---
+++ Assets/Scripts/FaceCursor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/** Causes the gameObject to rotate towards the player's cursor at a given speed. 
+ * Useful for turrets and anything else which should always turn to face the cursor.
+ */
+
+public class FaceCursor : MonoBehaviour {
+
+    // Turn speed still appears to be near instant at half maximum, without being truly instant.
+    [Range(0.001f, 0.5f)]
+    public float rotateSpeed;
+
+	private void FixedUpdate()
+	{
+        // mousePosition gives pixel coordinates, with (0, 0) at the bottom left
+        // of the screen. These must be translated to world coordinates before
+        // an accurate direction vector between the two points can be determined.
+        Vector3 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // Everything is converted to Vector2 in order to flatten the z components to 0.
+        Vector2 toCursor = cursorPos - transform.position;
+
+        // This limits the rotation possible in one frame, and therefore the 
+        // maximum turn speed.
+        float angle = Mathf.Clamp(Vector2.SignedAngle(transform.up, toCursor), 
+                                  rotateSpeed * -360, rotateSpeed * 360);
+        transform.Rotate(Vector3.forward, angle);
+	}
+}
---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+/** This singleton globally provides all variables directly relevant to the player.
+ * Any player stats, such as health, ammo, firerate, or consumables should be a 
+ * public variable in this instance. Anything that modifies these stats, whether 
+ * enemies or skills, should do so by referencing this class in a script of their own.
+ */
+
+public class PlayerData : MonoBehaviour {
+
+    public static PlayerData Instance { get; private set; }
+
+    public int health;
+    public float fireRate;
+    public float projectileSpeed;
+    public float pierceCount;
+
+
+	private void Awake()
+	{
+        // Singleton behavior prevents more than one instance of this class from
+        // existing.
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+	}
+
+	
+}
